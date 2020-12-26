869bd1fd925a18975c2e5557429046400fb4bab8
Gian Haenny <gian.andri.haenny@gmail.com>
2020-12-26 19:52:58 +0100

    add man in the middle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule, NbChatModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbChatModule, NbButtonModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { ChatComponent } from './chat/chat.component';
 
@@ -21,6 +21,7 @@ import { ChatComponent } from './chat/chat.component';
     NbLayoutModule,
     NbEvaIconsModule,
     NbChatModule,
+    NbButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
